4a4e88d0df790528e9f07932a9240055f995810c
thereisnocognac <yollovesxyra@gmail.com>
2022-05-27 02:19:43 -0400

    windows Javascripting

---
+++ functionIteration.js
@@ -0,0 +1,26 @@
+function newArr(x) {
+
+        if(newArr[0] < newArr.length-1) {
+            alert ('hi')
+        }else if(newArr[0] > newArr.length-1) {
+            alert ('Bye')
+        }else{
+            alert ('We close in an hour')
+        }
+    }
+
+
+newArr([1,2,3,4])
+
+
+
+
+
+function anotherArr(y) {
+    let newProduct = 1;
+    y.forEach((element, i) => {
+        product *= item
+    });
+}
+
+anotherArr([1,2,3,4]);
