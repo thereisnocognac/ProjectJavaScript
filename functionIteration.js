cf1544eab4493304bad84914fe9ba13b20b47adb
thereisnocognac <yollovesxyra@gmail.com>
2022-05-27 13:41:48 -0400

    triyng out stuff

--- functionIteration.js
+++ functionIteration.js
@@ -1,26 +1,39 @@
-function newArr(x) {
+// function newArr(x) {
 
-        if(newArr[0] < newArr.length-1) {
-            alert ('hi')
-        }else if(newArr[0] > newArr.length-1) {
-            alert ('Bye')
-        }else{
-            alert ('We close in an hour')
+//         if(newArr[0] < newArr.length-1) {
+//             alert ('hi')
+//         }else if(newArr[0] > newArr.length-1) {
+//             alert ('Bye')
+//         }else{
+//             alert ('We close in an hour')
+//         }
+//     }
+
+
+// newArr([1,2,3,4])
+
+
+
+
+
+// function anotherArr(y) {
+//     let newProduct = 1;
+//     y.forEach((element, i) => {
+//         product *= item
+//     });
+// }
+
+// anotherArr([1,2,3,4]);
+
+function yetAnotherArr(z) {
+    let allEven = [];
+    let pullItOut;
+
+    for(let i = 0; i < z.length; i++) {
+        if((z[i] % 2) === 0) {
+            allEven.push(z[i]);
         }
     }
-
-
-newArr([1,2,3,4])
-
-
-
-
-
-function anotherArr(y) {
-    let newProduct = 1;
-    y.forEach((element, i) => {
-        product *= item
-    });
 }
 
-anotherArr([1,2,3,4]);
+yetAnotherArr([1,2,3,4,5])
